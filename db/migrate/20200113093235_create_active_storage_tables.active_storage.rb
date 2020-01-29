c505d998bfdcf296b63b3d4e7bde1bac9f742874
Abdulraheem Sherif Adavuruku <aabdulraheemsherif@gmail.com>
2020-01-29 17:54:05 +0100

    addded record_type to active storage attachment

--- db/migrate/20200113093235_create_active_storage_tables.active_storage.rb
+++ db/migrate/20200113093235_create_active_storage_tables.active_storage.rb
@@ -15,8 +15,8 @@ class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
 
     create_table :active_storage_attachments do |t|
       t.string     :name,     null: false
-       t.references :blob,     null: false
-
+      t.references :blob,     null: false
+      t.string     :record_type,     null: false
       t.datetime :created_at, null: false
  
       t.index [ :record_type, :record_id, :name, :blob_id ], name: "index_active_storage_attachments_uniqueness", unique: true
