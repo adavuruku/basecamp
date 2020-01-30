fc4c678c7fb171e17f13831d94445a09e18d4a4c
Abdulraheem Sherif Adavuruku <aabdulraheemsherif@gmail.com>
2020-01-30 12:54:14 +0100

    FIX Error in test.rb

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,104 +1,102 @@
-# def goin
-#     return true
+# # def goin
+# #     return true
+# # end
+
+# # if goin
+# #     puts "coo"
+# # end
+
+# # j = 4
+
+# # if j%2 !=0
+# #     puts j
+# # end
+
+# # g ="gp"
+# # p g[0..20]
+
+# # 3 > 1
+# #     p "come"
+
+# # s = [10,5,3,11]
+
+# # if s == s.sort || s == s.sort.reverse
+# #     p "yes"
+# # else
+# #     p "no"
+# # end
+
+# # p "Welcome Home\nLittle".split("\s")
+
+
+# # #p Faker::Lorem.paragraph
+
+# # my_text = "Here is some basic text...\n...with a line break."
+
+# # simple_format(my_text)
+
+# # g = "%#{}%"
+
+
+# # d = [["go", "come"],["rat","toyyyy"]]
+# # g = []
+# # d.each do |f|
+# #     g.push(f.max) 
+# # end
+# # p d.map{|f| f.max}
+# # p g
+
+# # j = [2,4,5,6,7,8,9]
+
+# # h = j.include? 5 ? true : false
+# # p h
+# #  y = 9
+# # if y<5 then y=2 else y =3 end
+
+# # p y
+# # j = 
+# # @projectAttach = ProjectAttached.find_by(project_id: 18)
+# # @projectAttach.count
+
+
+
+# mount = valley = 0
+# counterM = counterV = 0
+# command = "UDDDUDUU".upcase.split("")
+# command.each do |st|
+#     if st=="U"
+#         counterM = counterM + 1
+#     end
+#     if st=="D"
+#         counterV = counterV + 1
+#     end
+
+#     if(counterM < counterV)
+#         counterV = counterV - 1
+#     end
+#     if(counterM < counterV)
+#         counterM = counterM - 1
+#     end
 # end
 
-# if goin
-#     puts "coo"
+# # p valley
+# q = 7
+
+# def maniCulate(q, d)
+#     jump = tot = 0 
+#     counter = 0
+#     d.each do |t|
+#         check = d[counter]
+#         if check == 1
+#             counter = counter + 2
+#         else
+#             counter = counter + 1
+#         end
+#         if d[counter] == 0
+#             jump = jump + 1
+#         end
+#     end
+#     p jump
 # end
 
-# j = 4
-
-# if j%2 !=0
-#     puts j
-# end
-
-# g ="gp"
-# p g[0..20]
-
-# 3 > 1
-#     p "come"
-
-# s = [10,5,3,11]
-
-# if s == s.sort || s == s.sort.reverse
-#     p "yes"
-# else
-#     p "no"
-# end
-
-# p "Welcome Home\nLittle".split("\s")
-
-
-# #p Faker::Lorem.paragraph
-
-# my_text = "Here is some basic text...\n...with a line break."
-
-# simple_format(my_text)
-
-# g = "%#{}%"
-
-
-# d = [["go", "come"],["rat","toyyyy"]]
-# g = []
-# d.each do |f|
-#     g.push(f.max) 
-# end
-# p d.map{|f| f.max}
-# p g
-
-# j = [2,4,5,6,7,8,9]
-
-# h = j.include? 5 ? true : false
-# p h
-#  y = 9
-# if y<5 then y=2 else y =3 end
-
-# p y
-# j = 
-# @projectAttach = ProjectAttached.find_by(project_id: 18)
-# @projectAttach.count
-
-
-
-mount = valley = 0
-counterM = counterV = 0
-command = "UDDDUDUU".upcase.split("")
-command.each do |st|
-    if st=="U"
-        counterM = counterM + 1
-    end
-    if st=="D"
-        counterV = counterV + 1
-    end
-
-    if(counterM < counterV)
-        counterV = counterV - 1
-    end
-    if(counterM < counterV)
-        counterM = counterM - 1
-    end
-end
-
-# p valley
-q = 7
-
-.
-
-def maniCulate(q, d)
-    jump = tot = 0 
-    counter = 0
-    d.each do |t|
-        check = d[counter]
-        if check == 1
-            counter = counter + 2
-        else
-            counter = counter + 1
-        end
-        if d[counter] == 0
-            jump = jump + 1
-        end
-    end
-    p jump
-end
-
-maniCulate(q, d)
+# maniCulate(q, d)
